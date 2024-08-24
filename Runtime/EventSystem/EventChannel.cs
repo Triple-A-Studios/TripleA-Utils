3ca0142fcda2646f4e22a37ec150d40d034fc7bb
Ali Qadir <94987050+ALI-QADIR@users.noreply.github.com>
2024-08-24 22:53:16 +0530

    Add Event Channel System

---
+++ Runtime/EventSystem/EventChannel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TripleA.EventSystem
+{
+	public abstract class EventChannel<T> : ScriptableObject
+	{
+		private readonly HashSet<EventListener<T>> m_observers = new();
+
+		public void Invoke(T value)
+		{
+			foreach (var observer in m_observers)
+			{
+				observer.Raise(value);
+			}
+		}
+
+		public void Register(EventListener<T> observer) => m_observers.Add(observer);
+
+		public void Unregister(EventListener<T> observer) => m_observers.Remove(observer);
+	}
+}
